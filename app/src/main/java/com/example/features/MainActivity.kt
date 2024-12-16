8d4a2db0e3966aa8aefe355accd0963e05cd7657
SauceSETeam <se@saucelabs.com>
2024-12-16 09:29:27 -0700

    Initial commit - Nav drawer populatead, empty screens

---
+++ app/src/main/java/com/example/features/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.features
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.features.ui.components.NavigationRailWithNavHost
+import com.example.features.ui.theme.FeaturesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FeatureApp()
+        }
+    }
+}
+
+@Composable
+fun FeatureApp() {
+    FeaturesTheme {
+        Surface(modifier = Modifier.fillMaxHeight(), color = MaterialTheme.colorScheme.background) {
+            NavigationRailWithNavHost()
+        }
+    }
+}
+
+@Preview(showBackground = true, apiLevel = 33)
+@Composable
+fun PreviewNavigationRail() {
+    FeaturesTheme {
+        NavigationRailWithNavHost()
+    }
+}
---
+++ app/src/main/java/com/example/features/ui/screens/MapScreen.kt
@@ -0,0 +1,9 @@
+package com.example.features.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MapScreen() {
+    Text(text = "Map will go here")
+}
